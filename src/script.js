224c2dda4ea9b948240679a6384496c09ac53aba
sueyoungshim <sueyoungshim@berkeley.edu>
2025-03-07 13:06:23 -0500

    copy and paste mediapipe demo from google

---
+++ src/script.js
@@ -0,0 +1,156 @@
+// Copyright 2023 The MediaPipe Authors.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//      http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {
+  HandLandmarker,
+  FilesetResolver
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+
+const demosSection = document.getElementById("demos");
+
+let handLandmarker = undefined;
+let runningMode = "IMAGE";
+let enableWebcamButton;
+let webcamRunning = false;
+
+const createHandLandmarker = async () => {
+  const vision = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+  );
+  handLandmarker = await HandLandmarker.createFromOptions(vision, {
+      baseOptions: {
+          modelAssetPath: `https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task`,
+          delegate: "GPU"
+      },
+      runningMode: runningMode,
+      numHands: 2
+  });
+  demosSection.classList.remove("invisible");
+};
+createHandLandmarker();
+
+const imageContainers = document.getElementsByClassName("detectOnClick");
+
+for (let i = 0; i < imageContainers.length; i++) {
+  imageContainers[i].children[0].addEventListener("click", handleClick);
+}
+
+async function handleClick(event) {
+  if (!handLandmarker) {
+      console.log("Wait for handLandmarker to load before clicking!");
+      return;
+  }
+
+  if (runningMode === "VIDEO") {
+      runningMode = "IMAGE";
+      await handLandmarker.setOptions({ runningMode: "IMAGE" });
+  }
+
+  const allCanvas = event.target.parentNode.getElementsByClassName("canvas");
+  for (let i = allCanvas.length - 1; i >= 0; i--) {
+      const n = allCanvas[i];
+      n.parentNode.removeChild(n);
+  }
+
+  const handLandmarkerResult = handLandmarker.detect(event.target);
+  console.log(handLandmarkerResult.handednesses[0][0]);
+  const canvas = document.createElement("canvas");
+  canvas.setAttribute("class", "canvas");
+  canvas.setAttribute("width", event.target.naturalWidth + "px");
+  canvas.setAttribute("height", event.target.naturalHeight + "px");
+  canvas.style =
+      "left: 0px;" +
+      "top: 0px;" +
+      "width: " + event.target.width + "px;" +
+      "height: " + event.target.height + "px;";
+
+  event.target.parentNode.appendChild(canvas);
+  const cxt = canvas.getContext("2d");
+  for (const landmarks of handLandmarkerResult.landmarks) {
+      drawConnectors(cxt, landmarks, HAND_CONNECTIONS, {
+          color: "#00FF00",
+          lineWidth: 5
+      });
+      drawLandmarks(cxt, landmarks, { color: "#FF0000", lineWidth: 1 });
+  }
+}
+
+const video = document.getElementById("webcam");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+
+const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
+
+if (hasGetUserMedia()) {
+  enableWebcamButton = document.getElementById("webcamButton");
+  enableWebcamButton.addEventListener("click", enableCam);
+} else {
+  console.warn("getUserMedia() is not supported by your browser");
+}
+
+function enableCam(event) {
+  if (!handLandmarker) {
+      console.log("Wait! objectDetector not loaded yet.");
+      return;
+  }
+
+  webcamRunning = !webcamRunning;
+  enableWebcamButton.innerText = webcamRunning ? "DISABLE PREDICTIONS" : "ENABLE PREDICTIONS";
+
+  const constraints = {
+      video: true
+  };
+
+  navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+      video.srcObject = stream;
+      video.addEventListener("loadeddata", predictWebcam);
+  });
+}
+
+let lastVideoTime = -1;
+let results = undefined;
+console.log(video);
+async function predictWebcam() {
+  canvasElement.style.width = video.videoWidth;
+  canvasElement.style.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvasElement.height = video.videoHeight;
+
+  if (runningMode === "IMAGE") {
+      runningMode = "VIDEO";
+      await handLandmarker.setOptions({ runningMode: "VIDEO" });
+  }
+  let startTimeMs = performance.now();
+  if (lastVideoTime !== video.currentTime) {
+      lastVideoTime = video.currentTime;
+      results = handLandmarker.detectForVideo(video, startTimeMs);
+  }
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  if (results.landmarks) {
+      console.log('landmarks', results.landmarks);
+      for (const landmarks of results.landmarks) {
+          drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+              color: "#00FF00",
+              lineWidth: 5
+          });
+          drawLandmarks(canvasCtx, landmarks, { color: "#FF0000", lineWidth: 2 });
+      }
+  }
+  canvasCtx.restore();
+
+  if (webcamRunning) {
+      window.requestAnimationFrame(predictWebcam);
+  }
+}
